00f3d944479ad78eed563868a6634806e914f75d
hargun79 <hargunsinghsahni@gmail.com>
2021-02-22 12:48:37 +0530

    Updated

--- src/Footer.js
+++ src/Footer.js
@@ -8,6 +8,7 @@ function Footer() {
        <a className="facebook" href="https://www.facebook.com/vedaqs/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
        <a className="slideshare" href="https://www.slideshare.net/QuizSocMSI" style={{marginLeft: '20px'}} target="_blank" rel="noopener noreferrer"><i className="fab fa-slideshare"></i></a>
        <a className="instagram" href="https://www.instagram.com/vedaquiz" style={{marginLeft: '20px'}} target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+       <a className="discord" href="https://discord.gg/yfvPavM4GK" style={{marginLeft: '20px'}} target="_blank" rel="noopener noreferrer"><i className="fab fa-discord"></i></a>
        <p style={{color: 'white'}}>Copyright © 2021 All Rights Reserved by Veda-The Quiz Society.</p>
      </div>
     </footer>
